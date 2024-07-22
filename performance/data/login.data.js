bd45a86400d4a3a9a7ecc7613bb18dd9cd29e200
Bruno Moleta <brunomoleta@pm.me>
2024-07-22 12:52:17 -0300

    :zap: performance: Add login performance tests
    This commit adds a new file for login performance tests, a login requests file and a data file for the credentials used in these tests.

---
+++ performance/data/login.data.js
@@ -0,0 +1,10 @@
+const password = 'psw!ebac@test';
+function generateRandomCharacter() {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    return chars[randomIndex];
+}
+
+const user = { email: `${generateRandomCharacter()}-${Math.random()}_ebac`, password };
+
+module.exports = { user };
---
+++ performance/tests/login.test.js
@@ -0,0 +1,25 @@
+const { group } = require('k6');
+const LoginRequest = require('../requests/login.request.js');
+const { user } = require('../data/login.data.js');
+
+export let options = {
+    stages: [
+        {duration: '20s', target: 20},
+        {duration: '2m', target: 20},
+        {duration: '20s', target: 0}
+    ]
+};
+
+export default function () {
+    const login = new LoginRequest()
+
+    group('Login não autorizado. Usuário não existe', () => {
+        login.unsuccessfulLogin(user)
+    })
+    group('Login com sucesso', () => {
+        login.successfulLogin(user)
+    })
+    group('Login não autorizado. Senha incorreta.', () => {
+        login.wrongPassword(user)
+    })
+}
