e0657b1122d4ef78bef322b0d14c5f7e369f9bf1
Bruno Moleta <brunomoleta@pm.me>
2024-07-23 13:40:58 -0300

    :broom: clean: format code

--- API/tests/postCoupon.data.mjs
+++ API/tests/postCoupon.data.mjs
@@ -1,38 +1,38 @@
-import {faker} from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import "dotenv/config";
 const token = process.env.API_TOKEN;
 
-export const apiRoute = "/coupons"
+export const apiRoute = "/coupons";
 
 export const newCoupon = {
-    code: `off${faker.word.noun()}${faker.number}`,
-    amount: `${faker.number.int({min: 1, max: 5000})}`,
-    discount_type: "fixed_product",
-    description: `Cupom de desconto de teste ${faker.color.human()}`,
+  code: `off${faker.word.noun()}${faker.number}`,
+  amount: `${faker.number.int({ min: 1, max: 5000 })}`,
+  discount_type: "fixed_product",
+  description: `Cupom de desconto de teste ${faker.color.human()}`,
 };
 export const existingCoupon = {
-    code: `20off`,
-    amount: `20`,
-    discount_type: "fixed_product",
-    description: `Cupom de desconto de teste`,
+  code: `20off`,
+  amount: `20`,
+  discount_type: "fixed_product",
+  description: `Cupom de desconto de teste`,
 };
 
 export const valid_discount_types = ["percent", "fixed_cart", "fixed_product"];
 
 export const errorMessage = {
-    noToken: 'Sem permissão para listar recursos.',
-    wrongBody: "Parâmetro(s) ausente(s)",
-    invalidName: "O código de cupom já existe"
-}
+  noToken: "Sem permissão para listar recursos.",
+  wrongBody: "Parâmetro(s) ausente(s)",
+  invalidName: "O código de cupom já existe",
+};
 export const scenarios = [
-    {
-        name: 'Quando é feita uma requisição sem token de autenticação',
-        auth: false,
-        errorCode: 401,
-    },
-    {
-        name: 'Quando é feita uma requisição com token autenticado porém sem autorização',
-        auth: `${token}asdas`,
-        errorCode: 401,
-    },
+  {
+    name: "Quando é feita uma requisição sem token de autenticação",
+    auth: false,
+    errorCode: 401,
+  },
+  {
+    name: "Quando é feita uma requisição com token autenticado porém sem autorização",
+    auth: `${token}asdas`,
+    errorCode: 401,
+  },
 ];
--- API/tests/postCoupons.spec.mjs
+++ API/tests/postCoupons.spec.mjs
@@ -1,118 +1,116 @@
 import "dotenv/config";
 import request from "supertest";
-import {expect} from "chai";
+import { expect } from "chai";
 import {
-    apiRoute,
-    errorMessage,
-    existingCoupon,
-    newCoupon,
-    scenarios,
-    valid_discount_types
+  apiRoute,
+  errorMessage,
+  existingCoupon,
+  newCoupon,
+  scenarios,
+  valid_discount_types,
 } from "./postCoupon.data.mjs";
 
 describe("Dado que você está fazendo um POST na rota de cupons da API", function () {
-    const apiUrl = process.env.API_URL;
-    const token = process.env.API_TOKEN;
+  const apiUrl = process.env.API_URL;
+  const token = process.env.API_TOKEN;
 
-    const {invalidName, noToken, wrongBody} = errorMessage
-    context(
-        "Quando é enviado um corpo de requisição inválido",
-        function () {
-            let response;
-            before(async function () {
-                response = await request(apiUrl)
-                    .post(apiRoute)
-                    .set("Authorization", `Bearer ${token}`)
-                    .send({});
-            });
-            it("Deve retornar mensagem de erro que auxilie o cliente.", function () {
-                const jsonData = response.body;
-
-                expect(jsonData).to.have.property("message");
-                expect(typeof jsonData.message).to.eql("string");
-                expect(jsonData.message).to.include(wrongBody);
-
-                expect(jsonData).to.have.property("data");
-                expect(jsonData.data).to.have.property("params");
-            });
-            it("Deve retornar status 400", function () {
-                expect(response.status).to.equal(400);
-            });
-        },
-    );
-    context("Quando é enviado um corpo com um nome de código que já existe.", function () {
-        let response;
-        before(async function () {
-            response = await request(apiUrl)
-                .post(apiRoute)
-                .set("Authorization", `Bearer ${token}`)
-                .send(existingCoupon);
-        });
-        it("Deve retornar mensagem de erro", function () {
-            const jsonData = response.body;
-
-            expect(jsonData).to.have.property("message");
-            expect(jsonData.message).to.be.a("string");
-            expect(jsonData.message).to.contain(invalidName);
-        })
-        it("Deve retornar status 400", function () {
-
-            expect(response.statusCode).to.equal(400);
-        });
-    })
-    context(
-        "Quando são enviados dados válidos com token autenticado",
-        function () {
-            let response;
-            before(async function () {
-                response = await request(apiUrl)
-                    .post(apiRoute)
-                    .set("Authorization", `Bearer ${token}`)
-                    .send(newCoupon);
-            });
-            it("Deve retornar o objeto com os itens esperados (CONTRATO).", function () {
-                const jsonData = response.body;
-
-                expect(jsonData).to.have.property("id");
-                expect(jsonData.id).to.be.a("number");
-                expect(jsonData).to.have.property("amount");
-                expect(jsonData.amount).to.be.a("string");
-                expect(jsonData).to.have.property("discount_type");
-                expect(valid_discount_types).to.include(jsonData.discount_type);
-                expect(jsonData).to.have.property("description");
-                expect(jsonData.description).to.be.a("string");
-            });
-            it("Deve retornar status 201", function () {
-                expect(response.statusCode).to.equal(201);
-            });
-        },
-    );
-
-    scenarios.forEach((scenario) => {
-        context(scenario.name, function () {
-            let response;
-            before(async function () {
-                if (!scenario.auth) {
-                    response = await request(apiUrl).get(apiRoute)
-                        .send(newCoupon);
-                } else {
-                    response = await request(apiUrl)
-                        .get(apiRoute)
-                        .set("Authorization", `Bearer ${scenario.auth}`)
-                        .send(newCoupon);
-                }
-            });
-            it("Deve retornar mensagem de erro que orienta o cliente", function () {
-                const jsonData = response.body;
-
-                expect(jsonData).to.have.property("message");
-                expect(jsonData.message).to.be.a("string");
-
-                expect(jsonData.message).to.contain(noToken);
-            })
-            it("Deve retornar status 401", function () {
-                expect(response.statusCode).to.equal(401);
-            });
-        })
+  const { invalidName, noToken, wrongBody } = errorMessage;
+  context("Quando é enviado um corpo de requisição inválido", function () {
+    let response;
+    before(async function () {
+      response = await request(apiUrl)
+        .post(apiRoute)
+        .set("Authorization", `Bearer ${token}`)
+        .send({});
     });
+    it("Deve retornar mensagem de erro que auxilie o cliente.", function () {
+      const jsonData = response.body;
+
+      expect(jsonData).to.have.property("message");
+      expect(typeof jsonData.message).to.eql("string");
+      expect(jsonData.message).to.include(wrongBody);
+
+      expect(jsonData).to.have.property("data");
+      expect(jsonData.data).to.have.property("params");
+    });
+    it("Deve retornar status 400", function () {
+      expect(response.status).to.equal(400);
+    });
+  });
+  context(
+    "Quando é enviado um corpo com um nome de código que já existe.",
+    function () {
+      let response;
+      before(async function () {
+        response = await request(apiUrl)
+          .post(apiRoute)
+          .set("Authorization", `Bearer ${token}`)
+          .send(existingCoupon);
+      });
+      it("Deve retornar mensagem de erro", function () {
+        const jsonData = response.body;
+
+        expect(jsonData).to.have.property("message");
+        expect(jsonData.message).to.be.a("string");
+        expect(jsonData.message).to.contain(invalidName);
+      });
+      it("Deve retornar status 400", function () {
+        expect(response.statusCode).to.equal(400);
+      });
+    },
+  );
+  context(
+    "Quando são enviados dados válidos com token autenticado",
+    function () {
+      let response;
+      before(async function () {
+        response = await request(apiUrl)
+          .post(apiRoute)
+          .set("Authorization", `Bearer ${token}`)
+          .send(newCoupon);
+      });
+      it("Deve retornar o objeto com os itens esperados (CONTRATO).", function () {
+        const jsonData = response.body;
+
+        expect(jsonData).to.have.property("id");
+        expect(jsonData.id).to.be.a("number");
+        expect(jsonData).to.have.property("amount");
+        expect(jsonData.amount).to.be.a("string");
+        expect(jsonData).to.have.property("discount_type");
+        expect(valid_discount_types).to.include(jsonData.discount_type);
+        expect(jsonData).to.have.property("description");
+        expect(jsonData.description).to.be.a("string");
+      });
+      it("Deve retornar status 201", function () {
+        expect(response.statusCode).to.equal(201);
+      });
+    },
+  );
+
+  scenarios.forEach((scenario) => {
+    context(scenario.name, function () {
+      let response;
+      before(async function () {
+        if (!scenario.auth) {
+          response = await request(apiUrl).get(apiRoute).send(newCoupon);
+        } else {
+          response = await request(apiUrl)
+            .get(apiRoute)
+            .set("Authorization", `Bearer ${scenario.auth}`)
+            .send(newCoupon);
+        }
+      });
+      it("Deve retornar mensagem de erro que orienta o cliente", function () {
+        const jsonData = response.body;
+
+        expect(jsonData).to.have.property("message");
+        expect(jsonData.message).to.be.a("string");
+
+        expect(jsonData.message).to.contain(noToken);
+      });
+      it("Deve retornar status 401", function () {
+        expect(response.statusCode).to.equal(401);
+      });
+    });
+  });
 });
