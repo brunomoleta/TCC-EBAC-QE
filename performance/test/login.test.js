8893f820983a8f21ca29065ca9de2e961d8e9b7f
Bruno Moleta <brunomoleta@pm.me>
2024-07-31 13:26:47 -0300

    :sparkles: feat: update k6 performance tests

--- performance/test/login.test.js
+++ performance/test/login.test.js
@@ -1,26 +1,27 @@
 const { group } = require("k6");
 const LoginRequest = require("../requests/login.request.js");
-const { user } = require("../data/login.data.js");
+const { successUser, randomUser } = require("../data/login.data.js");
 
 export let options = {
   stages: [
     { duration: "0s", target: 0 },
     { duration: "20s", target: 20 },
-    { duration: "2m", target: 20 },
+    { duration: "120s", target: 20 },
     { duration: "20s", target: 0 },
   ],
 };
 
 export default function () {
   const login = new LoginRequest();
+  group("Login com sucesso", () => {
+    login.successfulLogin(successUser);
+  });
 
   group("Login não autorizado. Usuário não existe", () => {
-    login.unsuccessfulLogin(user);
+    login.unsuccessfulLogin(randomUser);
   });
-  group("Login com sucesso", () => {
-    login.successfulLogin(user);
-  });
+
   group("Login não autorizado. Senha incorreta.", () => {
-    login.wrongPassword(user);
+    login.wrongPassword(successUser.email, successUser.password);
   });
 }
